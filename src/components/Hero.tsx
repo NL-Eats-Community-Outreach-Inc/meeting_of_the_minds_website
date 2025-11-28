e183b788037f60adc504b39515d77c41020f5a4e
Freddie-Pike <freddiepike709@gmail.com>
2025-11-28 00:06:23 -0330

    Change tagline.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -47,7 +47,7 @@ export const Hero = () => {
           </h1>
 
           <p className="text-2xl md:text-3xl text-background/90 font-medium">
-            120 of St. John's sharpest minds but only one team can be victorious.
+            120 bright minds. One cause. One champion team.
           </p>
 
           {/* Event Details */}
