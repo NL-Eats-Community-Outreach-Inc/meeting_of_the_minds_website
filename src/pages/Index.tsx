da20fe049b0759be9c6b2dd8b16a42c4120edc15
BCoishous <Brandon.Coish@gmail.com>
2026-03-30 18:49:58 -0230

    Remove Schedule Component, and stray comment

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,7 +6,6 @@ import { Prizes } from "@/components/Prizes";
 import { Sponsors } from "@/components/Sponsors";
 import { NLEats } from "@/components/NLEats";
 import { Hosts } from "@/components/Hosts";
-//import { Schedule } from "@/components/Schedule";
 import { Volunteer } from "@/components/Volunteer";
 import { Footer } from "@/components/Footer";
 
